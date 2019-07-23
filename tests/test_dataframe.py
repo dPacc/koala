e86b8d6c8cc322af569214ccac782963fc9234b6
dPacc <themdk01@gmail.com>
2019-07-23 16:21:27 +0400

    add one column and multiple column tests

--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -13,3 +13,16 @@ c = np.random.rand(3)
 d = np.array([True, False, True])
 e = np.array([1, 2, 3])
 df = koa.DataFrame({'a': a, 'b': b, 'c': c, 'd': d, 'e': e})
+
+
+class TestSelection:
+
+    def test_one_column(self):
+        assert_array_equal(df['a'].values[:, 0], a)
+        assert_array_equal(df['c'].values[:, 0], c)
+
+    def test_multiple_columns(self):
+        cols = ['a', 'c']
+        df_result = df[cols]
+        df_answer = pdc.DataFrame({'a': a, 'c': c})
+        assert_df_equals(df_result, df_answer)
