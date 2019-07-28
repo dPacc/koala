9c7c894ee8e04bbd89d1c702d16b8e820a3d7b3f
dPacc <themdk01@gmail.com>
2019-07-28 12:14:58 +0400

    add cumsum nonagg method

--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -321,3 +321,9 @@ class TestNonAgg:
         df_answer = pdc.DataFrame({'a': np.array([-11, 5, 5]),
                                    'b': np.array([3.4, 5.1, 5.1])})
         assert_df_equals(df_result, df_answer)
+
+    def test_cumsum(self):
+        df_result = df42.cumsum()
+        df_answer = pdc.DataFrame({'a': np.array([-11, -6, -3]),
+                                   'b': np.array([3.4, 8.5, 2.5])})
+        assert_df_equals(df_result, df_answer)
